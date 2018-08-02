8ba0b09206132a06584fd903b8c75046478dd9fe
lampayan <francis.lampayan@nokia.com>
2018-08-02 16:01:18 +0200

    working initialize request. WIP handling response

--- src/main/java/com/fbdl/crypto/fbdlcrypto/cmp/Main.java
+++ src/main/java/com/fbdl/crypto/fbdlcrypto/cmp/Main.java
@@ -14,10 +14,8 @@ import java.math.BigInteger;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
-import java.util.Date;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -28,7 +26,6 @@ import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.cmp.PKIBody;
 import org.bouncycastle.asn1.crmf.CertReqMessages;
-import org.bouncycastle.asn1.crmf.CertTemplateBuilder;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
@@ -45,7 +42,6 @@ import org.bouncycastle.cert.crmf.CertificateRequestMessage;
 import org.bouncycastle.cert.crmf.CertificateRequestMessageBuilder;
 import org.bouncycastle.cert.crmf.PKMACBuilder;
 import org.bouncycastle.cert.crmf.jcajce.JcePKMACValuesCalculator;
-import org.bouncycastle.crypto.tls.CertificateRequest;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMException;
 import org.bouncycastle.openssl.PEMParser;
@@ -76,7 +72,6 @@ public class Main {
         
         PrivateKey privateKey = converter.getPrivateKey(privateKeyInfo);
         PublicKey publicKey = converter.getPublicKey(subjectPublicKeyInfo);
-        
         
         //target CA and Client's CN
         X509NameEntryConverter dnConverter = new X509DefaultEntryConverter();
@@ -121,8 +116,8 @@ public class Main {
         //transactionId
         pbuilder.setTransactionID("1".getBytes());
         
-        //KeyID
-        pbuilder.setSenderKID("keyId".getBytes());
+        //KeyID: key id in CA
+        pbuilder.setSenderKID("54980".getBytes());
         
         CertReqMessages msgs = new CertReqMessages(message.toASN1Structure());
         PKIBody pkiBody = new PKIBody(PKIBody.TYPE_INIT_REQ, msgs);
@@ -132,12 +127,12 @@ public class Main {
         JcePKMACValuesCalculator jcePkmacCalc = new JcePKMACValuesCalculator();
         
         AlgorithmIdentifier digAlg = new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.3.14.3.2.26"));
-        AlgorithmIdentifier macAlg = new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.2.840.113549.2.7"));
+        AlgorithmIdentifier macAlg = new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.3.6.1.5.5.8.1.2"));
         
         jcePkmacCalc.setup(digAlg, macAlg);
         
         PKMACBuilder macbuilder = new PKMACBuilder(jcePkmacCalc);
-        MacCalculator macCalculator = macbuilder.build("54980:this_is_very_secret".toCharArray());
+        MacCalculator macCalculator = macbuilder.build("this_is_very_secret".toCharArray()); //PSK of keyid in CA
         
 //        ContentSigner msgSigner = new JcaContentSignerBuilder("sha1WithRSAEncryption").setProvider("BC").build(privateKey);
         ProtectedPKIMessage m = pbuilder.build(macCalculator);
