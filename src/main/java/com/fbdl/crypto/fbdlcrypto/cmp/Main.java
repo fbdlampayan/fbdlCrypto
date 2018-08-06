23cc8761173fdcc91b1314d66978c246fe6d141f
lampayan <francis.lampayan@nokia.com>
2018-08-06 12:18:12 +0200

    with extraction of certificate and print to PEM formatted String.

--- src/main/java/com/fbdl/crypto/fbdlcrypto/cmp/Main.java
+++ src/main/java/com/fbdl/crypto/fbdlcrypto/cmp/Main.java
@@ -10,10 +10,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.math.BigInteger;
 import java.security.PrivateKey;
+import java.security.Provider;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.ThreadLocalRandom;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -23,8 +30,16 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.cmp.CMPCertificate;
+import org.bouncycastle.asn1.cmp.CertOrEncCert;
+import org.bouncycastle.asn1.cmp.CertRepMessage;
+import org.bouncycastle.asn1.cmp.CertResponse;
+import org.bouncycastle.asn1.cmp.CertifiedKeyPair;
 import org.bouncycastle.asn1.cmp.PKIBody;
+import org.bouncycastle.asn1.cmp.PKIHeader;
+import org.bouncycastle.asn1.cmp.PKIMessage;
 import org.bouncycastle.asn1.crmf.CertReqMessages;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x500.X500Name;
@@ -46,6 +61,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMException;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.MacCalculator;
 import org.bouncycastle.operator.OperatorCreationException;
@@ -57,11 +73,15 @@ import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
  */
 public class Main {
     
-    public static void main(String[] args) throws PEMException, IOException, CRMFException, CMPException, OperatorCreationException {
+    public static void main(String[] args) throws PEMException, IOException, CRMFException, CMPException, OperatorCreationException, CertificateException {
         System.out.println("cmp-client!");
         
         //loading public and private
-        Security.addProvider(new BouncyCastleProvider());
+        final Provider bcProvider = new BouncyCastleProvider();
+        Security.addProvider(bcProvider);
+        
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509", bcProvider);
+        
         JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
         
         PEMParser privateKeyPemParser = new PEMParser(new InputStreamReader(new FileInputStream(new File("/home/fbdl/Desktop/VBXShared/keys/private_key.pem"))));
@@ -114,7 +134,9 @@ public class Main {
         pbuilder.setSenderNonce(senderNonce);
         
         //transactionId
-        pbuilder.setTransactionID("1".getBytes());
+        Integer x = new Integer(ThreadLocalRandom.current().nextInt(4, 100));
+        System.out.println("request trid: " + x);
+        pbuilder.setTransactionID(x.toString().getBytes());
         
         //KeyID: key id in CA
         pbuilder.setSenderKID("54980".getBytes());
@@ -137,7 +159,7 @@ public class Main {
 //        ContentSigner msgSigner = new JcaContentSignerBuilder("sha1WithRSAEncryption").setProvider("BC").build(privateKey);
         ProtectedPKIMessage m = pbuilder.build(macCalculator);
         
-        String url = "http://172.17.0.6:8888/pkix/"; //pod: insta-1011466520-jp8zq
+        String url = "http://10.0.0.52:8888/pkix/"; //k8s service
         
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost post = new HttpPost(url);
@@ -148,15 +170,48 @@ public class Main {
         HttpEntity entityBody = new ByteArrayEntity(m.toASN1Structure().getEncoded());
         post.setEntity(entityBody);
         
-        
         CloseableHttpResponse response = httpClient.execute(post);
         try{
             System.out.println("status line" + response.getStatusLine());
             HttpEntity entity1 = response.getEntity();
             
             System.out.println(response.getAllHeaders().length);
-            System.out.println("response body:");
-            System.out.println(EntityUtils.toString(entity1));
+            System.out.println("response body: " + entity1.getContentLength());
+//            System.out.println(EntityUtils.toString(entity1));
+            
+            
+//            PKIMessage r = PKIMessage.getInstance(ASN1Primitive.fromByteArray(IOUtils.toByteArray(entity1.getContent())));
+//            System.out.println("R: " + new String(r.getBody().getEncoded()));
+
+            ASN1InputStream asn1InputStream = new ASN1InputStream(entity1.getContent());
+            final PKIMessage respObject = PKIMessage.getInstance(asn1InputStream.readObject());
+            
+            //header
+            final PKIHeader header = respObject.getHeader();
+            System.out.println("header: " + header.getTransactionID());
+            
+            //signer (the CA)
+            final X500Name name = X500Name.getInstance(header.getSender().getName());
+            System.out.println("CA: " + name.toString());
+            
+            //body
+            final PKIBody body = respObject.getBody();
+            final CertRepMessage c = (CertRepMessage) body.getContent();
+            final CertResponse resp = c.getResponse()[0];
+            final CertifiedKeyPair kp = resp.getCertifiedKeyPair();
+            final CertOrEncCert cc = kp.getCertOrEncCert();
+            final CMPCertificate cmpcert = cc.getCertificate();
+            final byte encoded[] = cmpcert.getEncoded();
+            final X509Certificate cert = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(encoded));
+            
+            System.out.println("cert: " + new String(cert.getEncoded()).toLowerCase());
+            
+            StringWriter writer = new StringWriter();
+            JcaPEMWriter pemWriter = new JcaPEMWriter(writer);
+            pemWriter.writeObject(cert);
+            pemWriter.flush();
+            pemWriter.close();
+            System.out.println("content: " + writer.toString());
             
             EntityUtils.consume(entity1);
         } finally {
